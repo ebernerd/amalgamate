c8eae5815c5a3c0eddf0460379366c9aa4ac2b00
Eric Bernard <eric99b@gmail.com>
2021-05-03 00:17:12 -0400

    yay

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.scss"
 import { useContext } from "react"
-import { VideoModeContext } from "./contexts/VideoMode"
+import { PureVideoMode, VideoModeContext } from "./contexts/VideoMode"
 import { VideoModeSelector } from "./components/VideoModeSelector"
 import { VideoController } from "./components/VideoController"
 import { BoxSpinner } from "./components/BoxSpinner"
+
+const VIDEO_COUNTS: Record<PureVideoMode, number> = {
+	head: 24,
+	torso: 41,
+	leg: 37,
+}
 
 const App = () => {
 	//	Show the video mode selector if there is not one selected
@@ -11,7 +17,7 @@ const App = () => {
 	return videoMode === undefined ? (
 		<VideoModeSelector />
 	) : (
-		<BoxSpinner boxCount={5} videoMode={videoMode} />
+		<BoxSpinner boxCount={VIDEO_COUNTS[videoMode]} videoMode={videoMode} />
 	)
 }
 
--- frontend/src/components/BoxSpinner.tsx
+++ frontend/src/components/BoxSpinner.tsx
@@ -1,13 +1,28 @@
-import { useEffect, useMemo, useState, ReactNode } from "react"
+import React, {
+	useEffect,
+	useMemo,
+	useState,
+	ReactNode,
+	useDebugValue,
+	useCallback,
+} from "react"
 import { getRandInt } from "../utils/getRandInt"
 import Tween from "rc-tween-one"
 import { useWindowSize } from "../hooks/useWindowSize"
 import ReactPlayer from "react-player"
 import { PureVideoMode } from "../contexts/VideoMode"
-
+import io from "socket.io-client"
+import { useDebounce } from "use-debounce"
+import { Socket } from "dgram"
 export interface BoxSpinnerProps {
 	boxCount: number
 	videoMode: PureVideoMode
+}
+
+const NUM_TO_VIDEO_MODE: Record<number, PureVideoMode> = {
+	[1]: "head",
+	[2]: "torso",
+	[3]: "leg",
 }
 
 const COLORS = ["#03A9F4", "#4CAF50", "#f44336"]
@@ -17,31 +32,27 @@ export const BoxSpinner = (props: BoxSpinnerProps) => {
 
 	const [targetX, setTargetX] = useState<number>(0)
 	const [spinning, setSpinning] = useState<boolean>(false)
+	const [ready, setReady] = useState<boolean>(false)
+	const [socket, setSocket] = useState<any>()
+	const [buttonPressed, setButtonPressed] = useState<boolean>(false)
 	const resolvedWidth = width ?? 1920
 
 	const boxes: ReactNode[] = useMemo(() => {
 		let arr = []
 		for (let i = 0; i < props.boxCount; i++) {
 			arr.push(
-				<ReactPlayer
-					loop
-					playing
-					volume={0}
-					url={`videos/${props.videoMode}/${i + 1}.mp4`}
-					width={"100%"}
-					height={"100%"}
-				/>
+				<img src={`videos/${props.videoMode}/frames/${i + 1}.png`} />
 			)
 		}
 
 		return arr
 	}, [])
 
-	const tickInterval = 180
+	const tickInterval = 200
 	useEffect(() => {
+		//console.log(x, targetX, test)
 		const timerId = setInterval(() => {
 			if (x < targetX && spinning) {
-				console.log("woo!")
 				setX(x + 1)
 			} else {
 				setSpinning(false)
@@ -51,15 +62,54 @@ export const BoxSpinner = (props: BoxSpinnerProps) => {
 		return () => clearInterval(timerId)
 	})
 
+	const [debouncedReadyForSpin] = useDebounce(buttonPressed, 500)
+	useEffect(() => {
+		console.log(debouncedReadyForSpin)
+		if (debouncedReadyForSpin) {
+			setButtonPressed(false)
+			startSpinner()
+		}
+	}, [debouncedReadyForSpin])
+
+	const startSpinner = useCallback(() => {
+		setTargetX(targetX + getRandInt(5, Math.min(12, props.boxCount)))
+		setSpinning(true)
+	}, [props.videoMode, targetX])
+
+	const test = useMemo(() => targetX, [targetX])
+
+	const testCallback = (num: number) => {
+		if (NUM_TO_VIDEO_MODE[num] !== props.videoMode) {
+			return
+		}
+		setButtonPressed(true)
+	}
+
+	useEffect(() => {
+		const socket = io(`ws://${process.env.REACT_APP_WS}:5000`, {
+			withCredentials: false,
+		})
+		socket.on("connect", () =>
+			console.log("Websocket connection established.")
+		)
+		socket.on("input_event", testCallback)
+	}, [])
+
 	return (
 		<div>
 			<div>
 				{boxes.map((box, i) => (
 					<div
+						key={i}
 						className="box"
 						style={{
 							position: "fixed",
 							background: "white",
+							display:
+								spinning &&
+								Math.abs((x % props.boxCount) - i) <= 2
+									? "block"
+									: "none",
 							left:
 								x % props.boxCount === i
 									? 0
@@ -67,20 +117,31 @@ export const BoxSpinner = (props: BoxSpinnerProps) => {
 									? -resolvedWidth
 									: resolvedWidth,
 
-							zIndex: props.boxCount - i,
+							zIndex: props.boxCount - i + 100,
 						}}
 					>
 						{box}
 					</div>
 				))}
+				<ReactPlayer
+					url={`videos/${props.videoMode}/${
+						(targetX % props.boxCount) + 1
+					}.mp4`}
+					playing={true}
+					width="100vw"
+					height="100vh"
+					loop
+					on
+					style={{
+						zIndex: 1,
+					}}
+					volume={0}
+				/>
 			</div>
 
 			<button
 				onClick={() => {
-					setTargetX(
-						x + getRandInt(1, props.boxCount) + props.boxCount * 2
-					)
-					setSpinning(true)
+					startSpinner()
 				}}
 				style={{ position: "fixed", top: 500 }}
 			>
